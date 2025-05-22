35ebdfb637e64974581f704b0cef151e45662eeb
eternalgx <joseluizsff@gmail.com>
2025-05-22 17:41:58 -0300

    Update

---
+++ protected-mode/src/memory/heap/kheap.c
@@ -0,0 +1,32 @@
+#include "kheap.h"
+#include "heap.h"
+#include "config.h"
+#include "kernel.h"
+
+struct heap kernel_heap;
+struct heap_table kernel_heap_table;
+
+void kheap_init()
+{
+    int total_table_entries = PEACHOS_HEAP_SIZE_BYTES / PEACHOS_HEAP_BLOCK_SIZE;
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY*)(PEACHOS_HEAP_TABLE_ADDRESS); // unsigned char 0x00007E00
+    kernel_heap_table.total = total_table_entries;                                     // 25600
+
+    void* end = (void*)(PEACHOS_HEAP_ADDRESS + PEACHOS_HEAP_SIZE_BYTES);               // End of 100MB
+    int res = heap_create(&kernel_heap, (void*)(PEACHOS_HEAP_ADDRESS), end, &kernel_heap_table);
+    if (res < 0) // If has error
+    {
+        print("Failed to create heap\n");
+    }
+
+}
+
+void* kmalloc(size_t size)
+{
+    return heap_malloc(&kernel_heap, size);
+}
+
+void kfree(void* ptr)
+{
+    heap_free(&kernel_heap, ptr);
+}
